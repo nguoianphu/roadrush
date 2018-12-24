51abfe74ee4c1c3ed44f7e9a460bee1ac5c6f5f9
Stephen ChaoWen Lai <stephenlai2015@gmail.com>
2018-12-24 22:09:10 +0800

    2nd commit
    Upload project

---
+++ sw.js
@@ -0,0 +1,128 @@
+/*
+	We only need to modify 2 places:
+		1. version
+		2. filesToCache
+*/
+
+const version = '1.0'; /* Name your cache  */
+
+// register service worker
+// if service worker API is available
+// if (navigator.serviceWorker) {
+  if ('serviceWorker' in navigator) { 
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/sw.js', {scope: '/'})
+          .then(registration => {
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }).catch(err => {
+            console.log('ServiceWorker registration failed: ', err);
+        });
+    });
+  }
+  
+  const filesToCache = [				 /* Files you wan to store in cache */
+    '/',
+    '/index.html',
+    '/audio/boom.mp3',
+    '/audio/boom.ogg',
+    '/audio/random-race.mp3',
+    '/audio/random-race.ogg',
+    '/audio/whoosh.mp3',
+    '/audio/whoosh.ogg',
+    '/images/ui/buttons/1/1.png',
+    '/images/ui/buttons/1/2.png',
+    '/images/ui/buttons/1/3.png',
+    '/images/ui/buttons/1/4.png',
+    '/images/ui/buttons/1/5.png',
+    '/images/ui/buttons/1/6.png',
+    '/images/ui/buttons/2/1.png',
+    '/images/ui/buttons/2/2.png',
+    '/images/ui/buttons/2/3.png',
+    '/images/ui/buttons/2/4.png',
+    '/images/ui/buttons/2/5.png',
+    '/images/ui/buttons/2/6.png',
+    '/images/ui/icons/music_off.png',
+    '/images/ui/icons/music_on.png',
+    '/images/ui/icons/sfx_off.png',
+    '/images/ui/icons/sfx_on.png',
+    '/images/toggles/1.png',
+    '/images/toggles/2.png',
+    '/images/toggles/3.png',
+    '/images/toggles/4.png',
+    '/images/toggles/5.png',
+    '/images/toggles/6.png',
+    '/images/barrier.png',
+    '/images/cars.png',
+    '/images/cone.png',
+    '/images/line.png',
+    '/images/pcar1.png',
+    '/images/pcar2.png',
+    '/images/road.jpg',
+    '/images/title.png',
+    '/images/titleBack.jpg',
+    '/js/classes/comps/bar.js',
+    '/js/classes/comps/scoreBox.js',
+    '/js/classes/mc/controller.js',
+    '/js/classes/mc/model.js',
+    '/js/classes/ui/flatButtons.js',
+    '/js/classes/ui/soundButtons.js',
+    '/js/classes/ui/toggleButtons.js',
+    '/js/classes/util/align.js',
+    '/js/classes/util/alignGrid.js',
+    '/js/classes/util/collision.js',
+    '/js/classes/util/mediaManager.js',
+    '/js/classes/road.js',
+    '/js/classes/scenes/sceneLoad.js',
+    '/js/classes/scenes/sceneMain.js',
+    '/js/classes/scenes/sceneOver.js',
+    '/js/classes/scenes/sceneTitle.js',
+    '/js/constants.js',
+    '/js/main.js',
+    '/js/phaser.min.js',
+  ];
+  
+  // delete previous caches
+  self.addEventListener('activate', e => {
+    let cachecleaned = caches.keys().then(keys => {
+      keys.forEach(key => {
+        if(key !== `road-rush: v${version}` && key.match(`road-rush: v`)) 
+          return caches.delete(key)
+      })
+    })
+  })
+  
+  // install service worker 
+  self.addEventListener('install', e => {
+    console.log('sw install');
+    e.waitUntil(
+      caches.open(`road-rush: v${version}`)
+        .then(function(cache) {
+          console.log('sw caching files');
+          return cache.addAll(filesToCache);
+        }).catch(err => {
+          console.log(err);
+        })
+    );
+  });
+  
+  // Caching strategies
+  self.addEventListener('fetch', e => {
+    console.log('sw fetch');
+    console.log(e.request.url);
+
+    // 4. Cache with Network Update : 
+    e.respondWith(
+      caches.open(`road-rush: v${version}`)
+        .then(cache => {
+          return cache.match(e.request).then(res => {
+            let updateRes = fetch(e.request).then(newRes => {
+              cache.put(e.request, newRes.clone())
+              return newRes
+          })
+          return res || updateRes
+        })
+      })
+    )
+  });
+  
+  
